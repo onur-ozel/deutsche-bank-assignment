1569be673921a41e0d7647eecd6cd4c557c36c19
onur-ozel <onurozel41@gmail.com>
2023-06-11 22:39:27 +0200

    style: google style check changes

--- merchant/src/main/java/com/deutschebank/merchant/TradingApplication.java
+++ merchant/src/main/java/com/deutschebank/merchant/TradingApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TradingApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(TradingApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(TradingApplication.class, args);
+  }
 }
